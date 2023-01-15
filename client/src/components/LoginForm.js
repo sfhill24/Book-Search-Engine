d3d91032559a94d9f79af7694e669e8bbc524f15
Shelly Foxworth-Hill <sfhill24@gmail.com>
2023-01-15 17:24:03 -0500

    replaced addUser  API with ADD_USER muatation functionality

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -37,7 +37,7 @@ const LoginForm = () => {
       Auth.login(data.login.token)
 
     } catch (error) {
-      console.error(e);
+      console.error(error);
     }
 
     setUserFormData({
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -14,6 +14,9 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
+
+  const [addUser, { error }] = useMutation(ADD_USER);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -30,15 +33,15 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: {
+          username: userFormData.username,
+          password: userFormData.password,
+          email: userFormData.email
+        }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
