3c5a523ef5727ab73cccf80c2b64d50a74744198
Shelly Foxworth-Hill <sfhill24@gmail.com>
2023-01-16 00:03:26 -0500

    deploy to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://sfhill24:4GqgwW5yUVGxfER6@cluster0.tkuf7s1.mongodb.net/book-search?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
